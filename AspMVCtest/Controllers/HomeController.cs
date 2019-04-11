5239382ce94f39bf3736b505a0d0ebe99ff74391
wei <1103137217@gm.kuas.edu.tw>
2019-04-11 17:25:16 +0800

    加入專案檔案。

---
+++ AspMVCtest/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMVCtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.title = "hello world";
+            return View();
+        }
+
+
+        public class Student
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public int score { get; set; }
+            public Student()
+            {
+                id = string.Empty;
+                name = string.Empty;
+                score = 0;
+            }
+            public Student(string _id, string _name, int _score)
+            {
+                id = id;
+                name = name;
+                score = score;
+            }
+
+            public override string ToString()
+            {
+                return $"學號:{id},姓名:{name},分數:{score}.";
+            }
+
+
+
+        }
+
+
+    }
+
+    
+}
